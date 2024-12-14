f9c54094f7c435a1ff03b4a17f87a2cf789561e0
Nakul Srivastava <imnakul44@gmail.com>
2024-12-14 10:56:00 +0530

    Navbar Created + other intial Settings cLeaned up + Icons and title updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   return (
+      <>
+         <div className='bg-[linear-gradient(109.6deg,_rgb(9,_9,_121)_11.2%,_rgb(144,_6,_161)_53.7%,_rgb(0,_212,_255)_100.2%)] min-h-screen'>
+            <Navbar />
+         </div>
+      </>
+   )
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Navbar, Collapse, Typography, Avatar } from '@material-tailwind/react'
+import AvatarWithUserDropdown from './mini-components/Avatar'
+
+function NavList() {
+   return (
+      <ul className='my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-2'>
+         {/* <Typography
+            as='li'
+            variant='small'
+            color='blue-gray'
+            className='p-1 font-medium'
+         >
+            <a
+               href='#'
+               className='flex items-center hover:text-blue-500 transition-colors'
+            >
+               Pages
+            </a>
+         </Typography> */}
+         <AvatarWithUserDropdown />
+      </ul>
+   )
+}
+
+export function NavbarSimple() {
+   const [openNav, setOpenNav] = React.useState(false)
+
+   const handleWindowResize = () =>
+      window.innerWidth >= 960 && setOpenNav(false)
+
+   React.useEffect(() => {
+      window.addEventListener('resize', handleWindowResize)
+
+      return () => {
+         window.removeEventListener('resize', handleWindowResize)
+      }
+   }, [])
+
+   return (
+      <Navbar className='mx-auto max-w-screen-xl px-4 py-1'>
+         <div className='flex items-center justify-between text-blue-gray-900'>
+            <div className='flex justify-start gap-3'>
+               {/* icon */}
+               <img
+                  src='/online-interview.gif'
+                  alt=''
+                  className='contain h-10 w-10 border-2 border-black rounded-md'
+               />
+               {/* title  */}
+               <Typography
+                  as='a'
+                  href='#'
+                  variant='h5'
+                  className='mr-1 cursor-pointer py-1.5'
+               >
+                  Interview-Platform
+               </Typography>
+            </div>
+            {/* Navitemslist  */}
+            <div className='hidden lg:block'>
+               <NavList />
+            </div>
+         </div>
+         <Collapse open={openNav}>
+            <NavList />
+         </Collapse>
+      </Navbar>
+   )
+}
+
+export default NavbarSimple
